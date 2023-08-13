9a9d0a0a8aa9f167531182b3d7520b493a0f02ce
Rajat Kumar Jha <100137364+RAJAT123457@users.noreply.github.com>
2023-08-13 13:34:27 +0530

    Number guessing game

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,34 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+int main() {
+    int t;
+    srand(time(0));
+    t = rand()%100 + 1;
+    int n;
+    while(true){
+        cout<<"Guess a number between 1 and 100: "<<endl;
+        cin>>n;
+        if(cin.fail()){
+            cin.clear();
+            cin.ignore(10000,'\n');
+            cout<<"You entered invalid input, please enter valid input"<<endl;
+        }
+        else{
+            if(n==t){
+                cout<<"Hola! you guessed the correct number: "<<t<<endl;
+            }
+            else if(n<t){
+                cout<<"You guessed low number! Try again"<<endl;
+            }
+            else{
+                cout<<"You guessed high number! Try again"<<endl;
+            }
+        }
+    }
+    
+    return 0;
+}
